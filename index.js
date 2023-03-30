2d9c1d4d8d4dd6dfbaf039d78ab5f915283b837b
Pablo <pgarciacardone@gmail.com>
2023-03-31 00:30:25 +0100

    Add score, winner, rounds
    Remove duplicates of computer and player generating an extra round
    
    Notes:
    score within game func was causing issues due to return statement, also score addition of +1 was wrong syntax.
    add rounds to permit for winner func to show at end of game rather than at each end of round.

--- index.js
+++ index.js
@@ -4,32 +4,52 @@ function computerChoice() {
     return computer[random].toLowerCase();
 }
 
-let computer = computerChoice();
-let player = prompt("Select One: Rock, Paper, Scissors").toLowerCase();
 
-function round(player, computer) {
+
+let score = {
+     playerScore: 0,
+     computerScore: 0
+}
+
+function winner() {
+    if(score.playerScore > score.computerScore) {
+        return console.log("Player wins the game!");
+    } else if(score.playerScore < score.computerScore) {
+        return console.log("Computer wins the game!");
+    } else {
+        return console.log("It's a tie!");
+    }
+}
+
+function game(player, computer) {
     if (player === computer) {
-        return console.log("Tie");
+        console.log("Tie");
     } else {
         if (player === "rock" && computer === "scissors") {
-            return console.log("Player wins!");
+            console.log("Player wins!");
+             score.playerScore += 1;
         } else {
             if (player === "rock" && computer === "paper") {
-                return console.log("Computer Wins!");
+                console.log("Computer Wins!");
+                 score.computerScore += 1;
             } else {
                 if (player === "paper" && computer === "rock") {
-                    return console.log("Player Wins!");
+                     console.log("Player Wins!");
+                     score.playerScore += 1;
                 } else {
                     if (player === "paper" && computer === "scissors") {
-                        return console.log("Computer Wins!");
+                         console.log("Computer Wins!");
+                         score.computerScore += 1;
                     } else {
                         if (player === "scissors" && computer === "paper") {
-                            return console.log("Players Wins!");
+                             console.log("Players Wins!");
+                             score.playerScore += 1;
                         } else {
                             if (player === "scissors" && computer === "rock") {
-                                return console.log("Computer Wins!")
+                                 console.log("Computer Wins!")
+                                 score.computerScore += 1;
                             } else {
-                                return console.log("Error - Must enter a valid choice!");
+                                 console.log("Error - Must enter a valid choice!");
                             }
                         }
                     }
@@ -39,13 +59,23 @@ function round(player, computer) {
     }
 }
 
+let rounds = [];
 
 for(let i = 1; i <= 5; i++) {
+    computer = computerChoice();
+    player = prompt("Select One: Rock, Paper, Scissors").toLowerCase();
+    console.log(game(player, computer));
     console.log(`Round ${i}:`);
     console.log(`Player: ${player}`);
     console.log(`Computer: ${computer}`);
-    console.log(round(player, computer));
-    computer = computerChoice();
-    player = prompt("Select One: Rock, Paper, Scissors").toLowerCase();
+    console.log(`Player Score: ${score.playerScore}`);
+    console.log(`Computer Score: ${score.computerScore}`);
+    let result = i;
+    rounds.push(result);
 }
+
+if (rounds.length === 5) {
+    winner();
+}
+
 
