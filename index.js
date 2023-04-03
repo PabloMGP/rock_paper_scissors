ae7d3e0e27e733e03f60b68899055951a8977dd2
Pablo <pgarciacardone@gmail.com>
2023-04-03 02:25:18 +0100

    Add game result display, score counter, UI styling
    This commit adds the following features and improvements to the app:
    
    Display text for game winner and tie games
    Add visual score counter
    Add tie to score object to display tied games
    Add "play again" button with function to reset the game
    Finish adding functionality to RPS buttons
    Add comments for clarity
    Add basic CSS background colors and styling
    Add image logos for RPS player selection
    
    By implementing these improvements, the app is now more user-friendly
    and intuitive. The added functionality and visual elements enhance the
    user experience and make the app engaging enough for basic play.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Player RPS Selection - Button functionality
 rock = document.querySelector(".rock");
 
 rock.addEventListener("click", () => {
@@ -6,50 +7,55 @@ rock.addEventListener("click", () => {
 
 paper = document.querySelector(".paper");
 
-paper.addEventListener("click", () => {
-    paper = "paper";
-    computer = computerChoice();
-    game(paper, computer);
-    console.log();
+paper.addEventListener("click", (e) => {
+    playRound("paper");
 })
 
 scissors = document.querySelector(".scissors");
 
 scissors.addEventListener("click", () => {
-    scissors = "scissors";
-    computer = computerChoice();
-    game(scissors, computer);
-    console.log();
+    playRound("scissors");
 })
 
-
-
+// Computer RPS random selection
 function computerChoice() {
-    let computer = ["Rock", "Paper", "scissors"];
+    let computer = ["rock", "paper", "scissors"];
     let random = Math.floor(Math.random() * computer.length);
     return computer[random].toLowerCase();
-}
+}    
 
-
-
+// Score object
 let score = {
     playerScore: 0,
-    computerScore: 0
+    computerScore: 0,
+    tie: 0
 }
 
+// Visual score tracker
+function scoreCount() {
+    let playerScore = document.querySelector(".score_info");
+    playerScore.textContent = `Player: ${score.playerScore} | 
+    Computer: ${score.computerScore} | Tied: ${score.tie}`;
+}
+
+// Function to declare winner after 5 rounds
 function winner() {
+    const winner = document.querySelectorAll(".winner_player, .winner_computer, .tie")
+    document.querySelector(".score_info");
+    scoreCount.textContent = `Player ${score.playerScore}`;
     if (score.playerScore > score.computerScore) {
-        return console.log("Player wins the game!");
+        return winner[0].textContent = "You win the game!";
     } else if (score.playerScore < score.computerScore) {
-        return console.log("Computer wins the game!");
+        return winner[1].textContent = "Computer wins the game!";
     } else {
-        return console.log("It's a tie!");
+        return winner[2].textContent = "It's a tie!";
     }
 }
 
+// Main game logic
 function game(player, computer) {
     if (player === computer) {
-        console.log("Tie");
+        score.tie += 1;
     } else {
         if (player === "rock" && computer === "scissors") {
             console.log("Player wins!");
@@ -87,22 +93,60 @@ function game(player, computer) {
 
 
 
-
+// Round count and game info tracking function
 let roundCount = 0;
 
 function playRound(player) {
+    if (roundCount >= 5) {
+        return;
+    }
+
     const computer = computerChoice();
+    
     game(player, computer);
     roundCount++;
-    
-    console.log(`Round ${roundCount}:`);
-    console.log(`Player: ${player}`);
-    console.log(`Computer: ${computer}`);
-    console.log(`Player Score: ${score.playerScore}`);
-    console.log(`Computer Score: ${score.computerScore}`);
+    scoreCount();
 
-    if(roundCount === 5) {
-        winner();
-    } 
+    // To be replaced
+    const choices = document.querySelectorAll(".choices");
+    choices.forEach(element => {
+        element.textContent = `You chose: ${player} | Computer chose: ${computer}`;
+    });
+
+    // To be finished
+    // console.log(`Round ${roundCount}:`);
+    // console.log(`Player: ${player}`);
+    // console.log(`Computer: ${computer}`);
+    // console.log(`Player Score: ${score.playerScore}`);
+    // console.log(`Computer Score: ${score.computerScore}`);
+
+    // Stop game after 5 rounds are played
+    if (roundCount === 5) {
+        return winner();
+    }
 };
 
+
+// Reset game visual trackers & stored data
+const resetGame = document.querySelector(".reset_game");
+resetGame.addEventListener("click", () => {
+    reset();
+});
+
+
+function reset() {
+    // Reset the state of the function to its original values
+    score.playerScore = 0;
+    score.computerScore = 0;
+    score.tie = 0;
+    roundCount = 0;
+    scoreCount();
+    const winner = document.querySelectorAll(".winner_player, .winner_computer, .tie, .playerScore");
+    winner.forEach(element => {
+        element.textContent = "";
+        const choices = document.querySelectorAll(".choices");
+    choices.forEach(element => {
+        element.textContent = "";
+    });
+    });
+}
